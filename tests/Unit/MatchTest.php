921b195ef161704bae163a9aa65051ec79492118
Scott Pinkerton <ascottp@yahoo.co.uk>
2018-11-24 09:42:02 +0000

    Unit testing for scoreline

--- tests/Unit/MatchTest.php
+++ tests/Unit/MatchTest.php
@@ -237,4 +237,251 @@ class MatchTest extends TestCase
         $this->assertEquals('18-06-1996/west-germany-scotland', $match->url);
     }
 
+    /**
+     * Test the fixture for a home match.
+     *
+     * @return void
+     */
+    public function testHomeFixture()
+    {
+        $match = factory(Match::class)->make(['ha' => 'H']);
+
+        $this->assertEquals('Scotland v West Germany', $match->fixture);
+        $this->assertEquals('Scotland v W. Germany', $match->short_fixture);
+    }
+
+    /**
+     * Test the fixture for an away match.
+     *
+     * @return void
+     */
+    public function testAwayFixture()
+    {
+        $match = factory(Match::class)->make(['ha' => 'A']);
+
+        $this->assertEquals('West Germany v Scotland', $match->fixture);
+        $this->assertEquals('W. Germany v Scotland', $match->short_fixture);
+    }
+
+    /**
+     * Test the fixture for a neutral match with Scotland first.
+     *
+     * @return void
+     */
+    public function testNeutralHomeFixture()
+    {
+        $match = factory(Match::class)->make(['ha' => 'N1']);
+
+        $this->assertEquals('Scotland v West Germany', $match->fixture);
+        $this->assertEquals('Scotland v W. Germany', $match->short_fixture);
+    }
+
+    /**
+     * Test the fixture for a neutral match with Scotland second.
+     *
+     * @return void
+     */
+    public function testNeutralFixture()
+    {
+        $match = factory(Match::class)->make(['ha' => 'N']);
+
+        $this->assertEquals('West Germany v Scotland', $match->fixture);
+        $this->assertEquals('W. Germany v Scotland', $match->short_fixture);
+    }
+
+    /**
+     * Test the home team for a home match.
+     *
+     * @return void
+     */
+    public function testHomeHomeTeam()
+    {
+        $match = factory(Match::class)->make(['ha' => 'H']);
+
+        $this->assertEquals('Scotland', $match->home_team);
+    }
+
+    /**
+     * Test the home team for a neutral match with Scotland first.
+     *
+     * @return void
+     */
+    public function testNeutralHomeHomeTeam()
+    {
+        $match = factory(Match::class)->make(['ha' => 'N1']);
+
+        $this->assertEquals('Scotland', $match->home_team);
+    }
+
+    /**
+     * Test the home team for an away match.
+     *
+     * @return void
+     */
+    public function testAwayHomeTeam()
+    {
+        $match = factory(Match::class)->make(['ha' => 'A']);
+
+        $this->assertEquals('West Germany', $match->home_team);
+    }
+
+    /**
+     * Test the home team for a neutral match with Scotland first.
+     *
+     * @return void
+     */
+    public function testNeutralHomeTeam()
+    {
+        $match = factory(Match::class)->make(['ha' => 'N']);
+
+        $this->assertEquals('West Germany', $match->home_team);
+    }
+
+    /**
+     * Test the away team for a home match.
+     *
+     * @return void
+     */
+    public function testHomeAwayTeam()
+    {
+        $match = factory(Match::class)->make(['ha' => 'H']);
+
+        $this->assertEquals('West Germany', $match->away_team);
+    }
+
+    /**
+     * Test the away team for a neutral match with Scotland first.
+     *
+     * @return void
+     */
+    public function testNeutralHomeAwayTeam()
+    {
+        $match = factory(Match::class)->make(['ha' => 'N1']);
+
+        $this->assertEquals('West Germany', $match->away_team);
+    }
+
+    /**
+     * Test the away team for an away match.
+     *
+     * @return void
+     */
+    public function testAwayAwayTeam()
+    {
+        $match = factory(Match::class)->make(['ha' => 'A']);
+
+        $this->assertEquals('Scotland', $match->away_team);
+    }
+
+    /**
+     * Test the away team for a neutral match with Scotland first.
+     *
+     * @return void
+     */
+    public function testNeutralAwayTeam()
+    {
+        $match = factory(Match::class)->make(['ha' => 'N']);
+
+        $this->assertEquals('Scotland', $match->away_team);
+    }
+
+
+    /**
+     * Test the kickoff text.
+     *
+     * @return void
+     */
+    public function testKickoff()
+    {
+        $match = factory(Match::class)->make(['kickoff' => '20:00 (19:00 UK)']);
+
+        $this->assertEquals('20:00 (19:00 UK)', $match->kickoff);
+    }
+
+    /**
+     * Test the kickoff text for a blank value.
+     *
+     * @return void
+     */
+    public function testBlankKickoff()
+    {
+        $match = factory(Match::class)->make(['kickoff' => '']);
+
+        $this->assertEquals('unknown', $match->kickoff);
+    }
+
+
+
+    /**
+     * Test the H/A for H
+     *
+     * @return void
+     */
+    public function testHomeAwayH()
+    {
+        $match = factory(Match::class)->make(['ha' => 'H']);
+
+        $this->assertEquals('H', $match->home_away);
+    }
+
+    /**
+     * Test the H/A for A
+     *
+     * @return void
+     */
+    public function testHomeAwayA()
+    {
+        $match = factory(Match::class)->make(['ha' => 'A']);
+
+        $this->assertEquals('A', $match->home_away);
+    }
+
+    /**
+     * Test the H/A for N
+     *
+     * @return void
+     */
+    public function testHomeAwayN()
+    {
+        $match = factory(Match::class)->make(['ha' => 'N']);
+
+        $this->assertEquals('N', $match->home_away);
+    }
+
+    /**
+     * Test the H/A for N1
+     *
+     * @return void
+     */
+    public function testHomeAwayN1()
+    {
+        $match = factory(Match::class)->make(['ha' => 'N1']);
+
+        $this->assertEquals('N', $match->home_away);
+    }
+
+
+    /**
+     * Test the attendance text for a numerical value.
+     *
+     * @return void
+     */
+    public function testNumericalAttendance()
+    {
+        $match = factory(Match::class)->make(['attendance' => 52000]);
+
+        $this->assertEquals('52,000', $match->attendance);
+    }
+
+    /**
+     * Test the attendance text for a blank value.
+     *
+     * @return void
+     */
+    public function testBlankAttendance()
+    {
+        $match = factory(Match::class)->make(['attendance' => 0]);
+
+        $this->assertEquals('N/A', $match->attendance);
+    }
 }
