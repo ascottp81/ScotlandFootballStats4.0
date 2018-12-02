6f99ae7c7f1ae7d5000a83e335740614aa333c1b
Scott Pinkerton <ascottp@yahoo.co.uk>
2018-12-02 21:20:26 +0000

    Addition of Shark Bar in 12 Pubs

--- resources/views/other/pubs.blade.php
+++ resources/views/other/pubs.blade.php
@@ -35,18 +35,18 @@
 <div id="map"></div>
 <div class="info">
     <h2>Pubs</h2>
-    <p>1. The Bodega - 6:00 pm</p>
-    <p>2. Tilleys - 7:00 pm</p>
-    <p>3. Rafferty's - 7:30 pm</p>
-    <p>4. The Forth - 8:00 pm</p>
-    <p>5. The Town Wall - 8:30 pm</p>
-    <p>6. The Victoria Comet - 9:00 pm</p>
-    <p>7. The Centurion - 9:30 pm</p>
-    <p>8. Science Bar - 10:00 pm</p>
-    <p>9. Newcastle Tap - 10:30 pm</p>
-    <p>10. Head of Steam - 11:00 pm</p>
-    <p>11. The Union Rooms - 11:30 pm</p>
-    <p>12. Blackie Boy - 12:00 am</p>
+    <p>1. Shark Bar - 5:30 pm</p>
+    <p>2. The Bodega - 6:30 pm</p>
+    <p>3. Tilleys - 7:30 pm</p>
+    <p>4. Rafferty's - 8:00 pm</p>
+    <p>5. The Forth - 8:30 pm</p>
+    <p>6. The Town Wall - 9:00 pm</p>
+    <p>7. The Victoria Comet - 9:30 pm</p>
+    <p>8. The Centurion - 10:00 pm</p>
+    <p>9. Science Bar - 10:30 pm</p>
+    <p>10. Newcastle Tap - 11:00 pm</p>
+    <p>11. Head of Steam - 11:30 pm</p>
+    <p>12. The Union Rooms - 12:00 am</p>
 </div>
 <div class="rules">
     <h2>Rules</h2>
@@ -75,8 +75,8 @@
 
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 17,
-            center: {lat: 54.969777, lng: -1.617053 }
+            zoom: 16,
+            center: {lat: 54.971097, lng: -1.617053 }
         });
 
         // obtain data from back end
@@ -115,101 +115,101 @@
 
         // obtain the incident markers
         locations = [
+            {
+                "id": 1,
+                "location": {
+                    "lat": 54.973618,
+                    "lng": -1.622000
+                },
+                "details": "<h3>1. Shark Bar<\/h3><p>5:30 pm<\/p>Gallowgate, Newcastle upon Tyne NE1 4BT"
+            },
     {
-        "id": 1,
+        "id": 2,
         "location": {
             "lat": 54.970611,
             "lng": -1.62135
         },
-        "details": "<h3>1. The Bodega<\/h3><p>6:00 pm<\/p>125 Westgate Rd, Newcastle upon Tyne NE1 4AG"
-    },
-    {
-        "id": 2,
-        "location": {
-            "lat": 54.970488,
-            "lng": -1.620567
-        },
-        "details": "<h3>2. Tilleys<\/h3><p>7:00 pm<\/p>105 Westgate Rd, Newcastle upon Tyne NE1 4AF"
+        "details": "<h3>2. The Bodega<\/h3><p>6:00 pm<\/p>125 Westgate Rd, Newcastle upon Tyne NE1 4AG"
     },
     {
         "id": 3,
         "location": {
-            "lat": 54.96972,
-            "lng": -1.618837
+            "lat": 54.970488,
+            "lng": -1.620567
         },
-        "details": "<h3>3. Rafferty's<\/h3><p>7:30 pm<\/p>29-31 Pink Ln, Newcastle upon Tyne NE1 5DW"
+        "details": "<h3>3. Tilleys<\/h3><p>7:00 pm<\/p>105 Westgate Rd, Newcastle upon Tyne NE1 4AF"
     },
     {
         "id": 4,
         "location": {
-            "lat": 54.969595,
-            "lng": -1.618384
+            "lat": 54.96972,
+            "lng": -1.618837
         },
-        "details": "<h3>4. The Forth<\/h3><p>8:00 pm<\/p>Pink Ln, Newcastle upon Tyne NE1 5DW"
+        "details": "<h3>4. Rafferty's<\/h3><p>7:30 pm<\/p>29-31 Pink Ln, Newcastle upon Tyne NE1 5DW"
     },
     {
         "id": 5,
         "location": {
-            "lat": 54.969418,
-            "lng": -1.618438
+            "lat": 54.969595,
+            "lng": -1.618384
         },
-        "details": "<h3>5. The Town Wall<\/h3><p>8:30 pm<\/p>Pink Ln, Newcastle upon Tyne NE1 5HX"
+        "details": "<h3>5. The Forth<\/h3><p>8:00 pm<\/p>Pink Ln, Newcastle upon Tyne NE1 5DW"
     },
     {
         "id": 6,
         "location": {
-            "lat": 54.969356,
-            "lng": -1.617448
+            "lat": 54.969418,
+            "lng": -1.618438
         },
-        "details": "<h3>6. The Victoria Comet<\/h3><p>9:00 pm<\/p>38 Neville St, Newcastle upon Tyne NE1 5DF"
+        "details": "<h3>6. The Town Wall<\/h3><p>8:30 pm<\/p>Pink Ln, Newcastle upon Tyne NE1 5HX"
     },
     {
         "id": 7,
         "location": {
-            "lat": 54.969035,
-            "lng": -1.61639
+            "lat": 54.969356,
+            "lng": -1.617448
         },
-        "details": "<h3>7. The Centurion<\/h3><p>9:30 pm<\/p>Central Station, Neville Street, Newcastle upon Tyne NE1 5DG"
+        "details": "<h3>7. The Victoria Comet<\/h3><p>9:00 pm<\/p>38 Neville St, Newcastle upon Tyne NE1 5DF"
     },
     {
         "id": 8,
         "location": {
-            "lat": 54.969166,
-            "lng": -1.615813
+            "lat": 54.969035,
+            "lng": -1.61639
         },
-        "details": "<h3>8. Science Bar<\/h3><p>10:00 pm<\/p>Neville St, Newcastle upon Tyne NE1 5DH"
+        "details": "<h3>8. The Centurion<\/h3><p>9:30 pm<\/p>Central Station, Neville Street, Newcastle upon Tyne NE1 5DG"
     },
     {
         "id": 9,
         "location": {
-            "lat": 54.969506,
-            "lng": -1.615714
+            "lat": 54.969166,
+            "lng": -1.615813
         },
-        "details": "<h3>9. Newcastle Tap<\/h3><p>10:30 pm<\/p>8 Neville St, Newcastle upon Tyne NE1 5EN"
+        "details": "<h3>9. Science Bar<\/h3><p>10:00 pm<\/p>Neville St, Newcastle upon Tyne NE1 5DH"
     },
     {
         "id": 10,
         "location": {
-            "lat": 54.969521,
-            "lng": -1.615553
+            "lat": 54.969506,
+            "lng": -1.615714
         },
-        "details": "<h3>10. Head of Steam<\/h3><p>11:00 pm<\/p>2 Neville St, Newcastle upon Tyne NE1 5EN"
+        "details": "<h3>10. Newcastle Tap<\/h3><p>10:30 pm<\/p>8 Neville St, Newcastle upon Tyne NE1 5EN"
     },
     {
         "id": 11,
         "location": {
-            "lat": 54.969906,
-            "lng": -1.61493
+            "lat": 54.969521,
+            "lng": -1.615553
         },
-        "details": "<h3>11. The Union Rooms<\/h3><p>11:30 pm<\/p>48 Westgate Rd, Newcastle upon Tyne NE1 1TT"
+        "details": "<h3>11. Head of Steam<\/h3><p>11:00 pm<\/p>2 Neville St, Newcastle upon Tyne NE1 5EN"
     },
     {
         "id": 12,
         "location": {
-            "lat": 54.97038,
-            "lng": -1.612757
+            "lat": 54.969906,
+            "lng": -1.61493
         },
-        "details": "<h3>12. Blackie Boy<\/h3><p>12:00 am<\/p>11 Groat Market, Newcastle upon Tyne NE1 1UQ"
+        "details": "<h3>12. The Union Rooms<\/h3><p>11:30 pm<\/p>48 Westgate Rd, Newcastle upon Tyne NE1 1TT"
     }
 ];
 
@@ -218,7 +218,7 @@
     </script>
 
     <!-- Test Key -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDlPeo40hzrkvnQm91bv5E64GfZgYvF0UM &libraries=drawing,places&callback=initMap"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=&libraries=drawing,places&callback=initMap"></script>
 
 
 </body></html>
